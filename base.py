e3bd7585e1150fd61a145618b756b2cf612d1349
JamesPOliver <jamesjordanpo@gmail.com>
2023-05-22 15:49:42 +0100

    Added an output file for testing purposes

--- base.py
+++ base.py
@@ -1,5 +1,6 @@
 import pyaudio as pa
 import numpy as np
+import wave as wv
 
 audio = pa.PyAudio()
 
@@ -8,6 +9,13 @@ stream = audio.open(format=pa.paFloat32,
                     rate=44100,
                     input=True,
                     frames_per_buffer=1024)
+
+shout = 80
+
+output_file = wv.open('captured_sound.wav', 'wb')
+output_file.setnchannels(1)
+output_file.setsampwidth(audio.get_sample_size(pa.paFloat32))
+output_file.setframerate(44100)
 
 while True:
     data = stream.read(1024)
@@ -18,8 +26,12 @@ while True:
     decibels = 20 * np.log10(rms)
     print("Decibels: ", decibels)
 
-    if decibels > 20:
+    output_file.writeframes(data)
+
+    if decibels > shout or output_file.tell() > 44100 * 5:
+        output_file.close()
         stream.close()
         audio.terminate()
         print("Warning: Shouting can cause hypertension. How about you shut up for a minute, for your own health and everyone else's sanity?")
-
+    
+    
